9e961f9f1b41272dd5a2a29f313af675dbf6e39b
Karan3036 <karsingh9377@gmail.com>
2023-12-06 18:48:43 +0530

    Dec06(7PM)

--- force-app/main/default/aura/Post_to_QuickBook/Post_to_QuickBookController.js
+++ force-app/main/default/aura/Post_to_QuickBook/Post_to_QuickBookController.js
@@ -2,10 +2,54 @@
     doInit : function(component, event, helper) {
         var id = component.get("v.recordId");
         console.log(id);
+
         var Objectname = component.get("v.sobjecttype");
         console.log('Objectname---> ',Objectname);
-        if (Objectname == 'buildertek__Purchase_Order__c') {
-            helper.createPO(component, event, helper);
+
+        if(Objectname == "Account"){
+            component.set("v.ShowAccountTypeOpt", true);
         }
-    }
+
+        if (Objectname == 'buildertek__Purchase_Order__c') {
+            // helper.createPO(component, event, helper);
+        }
+    },
+
+    handleRecordLoaded: function(component, event, helper) {
+
+        console.log(component.get("v.BTAccountType.buildertek__BT_Account_Type__c"));
+        var BTAccountType = component.get("v.BTAccountType")["buildertek__BT_Account_Type__c"];
+        if(BTAccountType == "Customer" || BTAccountType == "Vendor"){
+            component.set("v.AccountType", BTAccountType)
+        }
+
+      },
+
+    handleChangeAccountType: function(component, event, helper){
+        var auraIdField = event.getSource().getLocalId();
+
+        console.log('Selected account type : ',component.find(auraIdField).get("v.value"));
+
+        component.set("v.AccountType",component.find(auraIdField).get("v.value"));
+    },
+
+    PostAccountToQuickbook: function(component, event, helper){
+        var accountType = component.get("v.AccountType");
+        if(accountType == 'Customer' || accountType == 'Vendor'){
+            component.set("v.ShowAccountTypeOpt", false);
+            helper.Post_Customer_vendor_ToQBHelper(component, event, helper);
+        }
+        else{
+            component.find('notifLib').showNotice({
+                "variant": "error",
+                "header": "Error",
+                "message": 'Please Select at least on Account Type!',
+            });    
+        }
+    },
+
+    CloseQuickAction: function(component, event, helper){
+        $A.get("e.force:closeQuickAction").fire();
+    },
+
 })
