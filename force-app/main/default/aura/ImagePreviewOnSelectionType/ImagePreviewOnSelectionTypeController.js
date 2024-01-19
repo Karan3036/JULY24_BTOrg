e5c5933ed0679bf86b06b58a3b0df0de62279c65
Karan3036 <karsingh9377@gmail.com>
2024-01-19 11:08:14 +0530

    Jan-19-2024

--- force-app/main/default/aura/ImagePreviewOnSelectionType/ImagePreviewOnSelectionTypeController.js
+++ force-app/main/default/aura/ImagePreviewOnSelectionType/ImagePreviewOnSelectionTypeController.js
@@ -3,7 +3,7 @@
         var recordId = component.get("v.recordId");
         console.log('recordId => ' + recordId);
 
-        var action = component.get("c.getProductFilesMap"); // Updated method name
+        var action = component.get("c.getProductFilesMap");
 
         action.setParams({
             recordId: recordId
@@ -16,13 +16,12 @@
             if (state === "SUCCESS") {
                 var result = response.getReturnValue();
                 console.log('Result =>', { result });
-
-                // Check if the result is not an error
-                if (result) {
-                    // Convert the map to a list of objects with key and value properties
+                if (result && Object.keys(result).length > 0) {
                     var contentDocsList = helper.convertMapToList(result);
+                    console.log('contentDocsList--->',contentDocsList);
                     component.set("v.contentDocsList", contentDocsList);
                 } else {
+                    component.set("v.displayImage", false);
                     console.error("Error fetching product files.");
                 }
             } else {
@@ -41,5 +40,31 @@
         } else {
             cmp.set('v.activeSectionsMessage', "Open sections: " + openSections.join(', '));
         }
-    }
+    },
+
+    handle_img_click: function(component, event, helper){
+        try {
+            console.log('in handle images-->');
+            var imageSrc = event.getSource().get("v.src");
+            var imageTitle = 'Image';
+            var imageId = event.getSource().get("v.id")
+
+            helper.changeImageHelper(component, event, helper, imageId, false);
+            helper.openCustomPreviewHelper(component, event, helper, imageSrc, imageTitle, imageId);
+        } catch (error) {
+            console.log('error-->',error);
+        }
+
+    },
+
+    doView: function(component, event, helper) {
+        ////console.log(event.currentTarget.dataset.record);
+        var editRecordEvent = $A.get("e.force:navigateToSObject");
+        console.log('event.currentTarget.dataset.record-->',event.currentTarget.dataset.record);
+        editRecordEvent.setParams({
+            "recordId": event.currentTarget.dataset.record
+        });
+        editRecordEvent.fire();
+    },
+
 });
--- force-app/main/default/aura/Product_Files/Product_FilesController.js
+++ force-app/main/default/aura/Product_Files/Product_FilesController.js
@@ -6,7 +6,7 @@
         var baseUrl = window.location.protocol + '//' + window.location.hostname + '/' + orgId.substring(0, 15);
         console.log('Organization Base URL: ' + baseUrl);
         component.set('v.orgBaseURL', baseUrl);
-
+        
         var action = component.get("c.getProductFiles");
         action.setParams({
             recordId: recordId
@@ -16,11 +16,56 @@
             console.log('Status =>', {state});
             var result = response.getReturnValue();
             console.log('Result =>', {result});
-            if (result.status != 'error') {
+            if (result.status == 'success') {
                 component.set("v.contentDocsList", result.contentLink);
                 console.log(component.get("v.contentDocsList"));
+            }else if(result.status == 'There Are No Product.'){
+                component.set("v.message", 'Please assign Product to the Selection Option.');
+            }else if(result.status == 'There Are No Files.') {
+                component.set("v.message", 'There are no option images for this Selection Option.');  
+            }else{
+                component.set("v.message", 'Something went wrong while fetching option images.'); 
             }
         });
         $A.enqueueAction(action);
+    },
+    handle_img_click: function(component, event, helper){
+
+        console.log("Inside click method");
+        var clickedDiv = event.currentTarget;
+
+        // Retrieve values from data attributes
+        var imageSrc = clickedDiv.getAttribute('data-src');
+        var imageId = clickedDiv.getAttribute('id');
+        var imageTitle = clickedDiv.getAttribute('data-description');
+        console.log('imageSrc' ,imageSrc);
+        console.log('imageId' ,imageId);
+        console.log('imageTitle',imageTitle);
+        helper.changeImageHelper(component, event, helper, imageId, false);  // To set Visibilti of Next - Previuos button for First & last img
+        helper.openCustomPreviewHelper(component, event, helper, imageSrc, imageTitle, imageId);
+
+    },
+    stopEventPropogation: function(component, event, helper){
+        event.stopPropagation();
+    },
+    closeImagePreview : function(component, event, helper){
+        component.set("v.Is_ImageHavePreview", false);
+        component.set('v.Show_ImagePreview', false);
+    },
+    Handle_imageLoaded: function(component, event, helper){
+        console.log('image loaded');
+        component.set("v.PreviewImgSpinner", false);
+    },
+    
+    Handle_imageNotLoaded: function(component, event, helper){
+        console.log('image not loaded');
+        component.set("v.Is_ImageHavePreview", false);
+        component.set("v.PreviewImgSpinner", false);
+
+    },
+    ChangeImg: function(component, event, helper){
+        component.set("v.Is_ImageHavePreview", false);
+        component.set('v.Show_ImagePreview', false);
+        helper.changeImageHelper(component, event, helper, null, true);
     },
 })
